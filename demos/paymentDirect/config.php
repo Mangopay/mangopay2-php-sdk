1f6ca71e52112c7942c19e8a2bba711e13eda27b
Łukasz Puślecki <lukasz.puslecki@mosaic-lab.com>
2013-09-13 15:11:23 +0200

    Remove BaseUrl from config files

--- demos/paymentDirect/config.php
+++ demos/paymentDirect/config.php
@@ -4,7 +4,6 @@
  */
 define('MangoPayDemo_ClientId', 'example');
 define('MangoPayDemo_ClientPassword', 'uyWsmnwMQyTnqKgi8Y35A3eVB7bGhqrebYqA1tL6x2vYNpGPiY');
-define('MangoPayDemo_BaseUrl', 'https://mangopay-api-inte.leetchi.com');
 
 /**
  * Path to folder with temporary files (with permissions to write)
