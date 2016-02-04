4a9de7410e3e5dacaeefd8b94b6d1b6af2141328
Hugo Bailey <hugo@mangopay.com>
2016-02-04 17:37:16 +0100

    Update Address for API v2.01

--- demos/workflow/scripts/bankaccount.php
+++ demos/workflow/scripts/bankaccount.php
@@ -6,7 +6,13 @@ $BankAccount->Details = new MangoPay\BankAccountDetailsIBAN();
 $BankAccount->Details->IBAN = "FR7618829754160173622224154";
 $BankAccount->Details->BIC = "CMBRFR2BCME";
 $BankAccount->OwnerName = "Joe Bloggs";
-$BankAccount->OwnerAddress = "1 Mangopay Street";
+$BankAccount->OwnerAddress = new \MangoPay\Address();
+$BankAccount->OwnerAddress->AddressLine1 = 'Address line 1';
+$BankAccount->OwnerAddress->AddressLine2 = 'Address line 2';
+$BankAccount->OwnerAddress->City = 'City';
+$BankAccount->OwnerAddress->Country = 'FR';
+$BankAccount->OwnerAddress->PostalCode = '11222';
+$BankAccount->OwnerAddress->Region = 'Region';
 $result = $mangoPayApi->Users->CreateBankAccount($UserId, $BankAccount);
 
 //Display result
