0db3f27218e2c0ab70af34917ca94f9dec29149b
Hugo Bailey <hobailey@users.noreply.github.com>
2016-06-28 15:48:12 +0200

    Remove new client creation tests

--- tests/cases/clients.php
+++ tests/cases/clients.php
@@ -6,22 +6,8 @@ require_once 'base.php';
  * Test class for Client API
  */
 class Clients extends Base {
-    
-   function test_ClientsCreateClient() {        
-        $id = rand(1000000000, 10000000000);
-        $client = $this->_api->Clients->create($id, "test",  "test@o2.pl");
-        $this->assertTrue("test" == $client->Name);
-        $this->assertTrue(strlen($client->Passphrase) > 0);
-    }
-    
-    function test_Clients_TryCreateInvalidClient() {
-        // invalid id
-        $this->expectException('MangoPay\Libraries\ResponseException');
-        $client = $this->_api->Clients->create("0", "test",  "test@o2.pl");
-        $this->assertTrue($client == null);
-    }
-	
-	function test_Clients_Get() {
+ 
+    function test_Clients_Get() {
         $getClients = $this->_api->Clients->Get();
         
         $this->assertEqual($this->_api->Config->ClientId, $getClients->ClientId);
