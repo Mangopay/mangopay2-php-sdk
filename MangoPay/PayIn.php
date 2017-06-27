562b5473041b023d014d6d8dfc847a63b9631a66
Gabriel Lupu <lupugabriel1@gmail.com>
2017-06-27 08:25:30 +0300

    Update properties and add missing model

--- MangoPay/PayIn.php
+++ MangoPay/PayIn.php
@@ -11,25 +11,25 @@ class PayIn extends Transaction
      * @var int
      */
     public $CreditedWalletId;
-    
+
     /**
      * PaymentType {CARD, BANK_WIRE, DIRECT_DEBIT, PAYPAL }
      * @var string
      */
     public $PaymentType;
-    
+
     /**
      * One of PayInPaymentDetails implementations, depending on $PaymentType
      * @var object
      */
     public $PaymentDetails;
-    
+
     /**
      * ExecutionType { WEB, TOKEN, DIRECT, PREAUTHORIZED, RECURRING_ORDER_EXECUTION }
      * @var string
      */
     public $ExecutionType;
-    
+
     /**
      * One of PayInExecutionDetails implementations, depending on $ExecutionType
      * @var object
@@ -49,6 +49,7 @@ class PayIn extends Transaction
                 PayInPaymentType::Preauthorized => '\MangoPay\PayInPaymentDetailsPreAuthorized',
                 PayInPaymentType::BankWire => '\MangoPay\PayInPaymentDetailsBankWire',
                 PayInPaymentType::DirectDebit => '\MangoPay\PayInPaymentDetailsDirectDebit',
+                PayInPaymentType::DirectDebitDirect => '\MangoPay\PayInPaymentDetailsDirectDebitDirect',
                 PayInPaymentType::PayPal => '\MangoPay\PayInPaymentDetailsPaypal',
                 // ...and more in future...
             ),
@@ -70,7 +71,7 @@ class PayIn extends Transaction
         $properties = parent::GetReadOnlyProperties();
         array_push($properties, 'PaymentType');
         array_push($properties, 'ExecutionType');
-        
+
         return $properties;
     }
 }
--- MangoPay/PayInPaymentType.php
+++ MangoPay/PayInPaymentType.php
@@ -9,6 +9,7 @@ class PayInPaymentType
     const BankWire = 'BANK_WIRE';
     const Card = 'CARD';
     const DirectDebit = 'DIRECT_DEBIT';
+    const DirectDebitDirect = 'DIRECT_DEBIT_DIRECT';
     const Preauthorized = 'PREAUTHORIZED';
 	const PayPal = 'PAYPAL';
 }
