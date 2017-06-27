f60ae1841f94271089983b1d7d6ce154c19637b9
Gabriel Lupu <lupugabriel1@gmail.com>
2017-06-27 08:50:31 +0300

    Wallet reporting and transactions reporting enhancements

--- tests/cases/reports.php
+++ tests/cases/reports.php
@@ -6,44 +6,55 @@ require_once 'base.php';
  * Tests basic methods for report requests
  */
 class Reports extends Base {
-    
-    function test_ReportRequest_Create() {
+
+    function test_ReportRequestTransactions_Create() {
         $reportRequest = new \MangoPay\ReportRequest();
         $reportRequest->ReportType = \MangoPay\ReportType::Transactions;
-        
+
         $createReportRequest = $this->_api->Reports->Create($reportRequest);
-        
+
         $this->assertNotNull($createReportRequest);
         $this->assertTrue($createReportRequest->Id > 0);
         $this->assertSame($createReportRequest->ReportType, \MangoPay\ReportType::Transactions);
     }
-    
+
+    function test_ReportRequestWallets_Create() {
+        $reportRequest = new \MangoPay\ReportRequest();
+        $reportRequest->ReportType = \MangoPay\ReportType::Wallets;
+
+        $createReportRequest = $this->_api->Reports->Create($reportRequest);
+
+        $this->assertNotNull($createReportRequest);
+        $this->assertTrue($createReportRequest->Id > 0);
+        $this->assertSame($createReportRequest->ReportType, \MangoPay\ReportType::Wallets);
+    }
+
     function test_ReportRequest_Get() {
         $reportRequest = new \MangoPay\ReportRequest();
         $reportRequest->ReportType = \MangoPay\ReportType::Transactions;
         $createReportRequest = $this->_api->Reports->Create($reportRequest);
-        
+
         $getReportRequest = $this->_api->Reports->Get($createReportRequest->Id);
-        
+
         $this->assertNotNull($getReportRequest);
         $this->assertSame($getReportRequest->Id, $createReportRequest->Id);
         $this->assertSame($getReportRequest->ReportType, $createReportRequest->ReportType);
     }
-    
+
     function test_ReportRequest_All() {
         $pagination = new \MangoPay\Pagination(1, 1);
-        
+
         $getAllReportRequests = $this->_api->Reports->GetAll($pagination);
 
         $this->assertEqual(count($getAllReportRequests), 1);
         $this->assertIsA($getAllReportRequests[0], '\MangoPay\ReportRequest');
     }
-    
+
     function test_ReportRequest_All_SortByCreationDate() {
         $pagination = new \MangoPay\Pagination(1, 1);
         $sorting = new \MangoPay\Sorting();
         $sorting->AddField("CreationDate", \MangoPay\SortDirection::DESC);
-        
+
         $getAllReportRequests = $this->_api->Reports->GetAll($pagination, null, $sorting);
 
         $this->assertEqual(count($getAllReportRequests), 1);
