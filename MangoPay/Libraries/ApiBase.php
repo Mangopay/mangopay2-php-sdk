01b767217195d91a4651612ea78c47bfafa3c8e2
Gabriel Lupu <lupugabriel1@gmail.com>
2017-06-27 09:18:12 +0300

    Update reports types

--- MangoPay/Libraries/ApiBase.php
+++ MangoPay/Libraries/ApiBase.php
@@ -463,7 +463,8 @@ abstract class ApiBase
             'payins_directdebit-direct_create' => '\MangoPay\PayIn',
             'payins_createrefunds' => '\MangoPay\Refund',
             'payouts_bankwire_create' => '\MangoPay\PayOut',
-            'reports_create' => '\MangoPay\ReportRequest',
+            'reports_transactions_create' => '\MangoPay\ReportRequest',
+            'reports_wallets_create' => '\MangoPay\ReportRequest',
             'transfers_createrefunds' => '\MangoPay\Refund',
             'transfers_create' => '\MangoPay\Transfer',
             'users_createnaturals' => '\MangoPay\UserNatural', // done
