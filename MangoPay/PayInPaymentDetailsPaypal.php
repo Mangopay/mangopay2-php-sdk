bf173c8706d9717f5811f6252b31e458ade518a9
Paul Hristea <paul.hristea@codegile.com>
2017-08-17 15:42:48 +0300

    Added the ShippingAddress field to the PayPal PayIn class, included it in a test. A few minor fixes.

--- MangoPay/PayInPaymentDetailsPaypal.php
+++ MangoPay/PayInPaymentDetailsPaypal.php
@@ -5,4 +5,18 @@ namespace MangoPay;
  */
 class PayInPaymentDetailsPaypal extends Libraries\Dto implements PayInPaymentDetails
 {
+    /**
+     * Address used instead of the user's PayPal account address.
+     * @var ShippingAddress
+     */
+    public $ShippingAddress;
+
+    public function GetSubObjects()
+    {
+        $subObjects = parent::GetSubObjects();
+
+        $subObjects['ShippingAddress'] = '\Mangopay\ShippingAddress';
+
+        return $subObjects;
+    }
 }
---
+++ MangoPay/ShippingAddress.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CodegileS1
+ * Date: 17.08.2017
+ * Time: 12:56
+ */
+
+namespace MangoPay;
+
+/**
+ * Class represents the shipping address of a PayPal PayIn.
+ */
+class ShippingAddress extends Libraries\Dto
+{
+    /**
+     * The recipient's name.
+     * @var string
+     */
+    public $RecipientName;
+
+    /**
+     * The address.
+     * @var Address
+     */
+    public $Address;
+
+    public function GetSubObjects()
+    {
+        $subObjects = parent::GetSubObjects();
+
+        $subObjects['Address'] = '\Mangopay\Address';
+
+        return $subObjects;
+    }
+}
