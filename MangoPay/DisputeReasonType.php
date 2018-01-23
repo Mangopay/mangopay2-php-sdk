90b36e706e22d53f8ed7b7faf40247d75a56377a
Mickaël POIS <mickael@mangopay.com>
2018-01-23 17:51:53 +0100

    Add 4 new and 10 missing DisputeReasonType entries

--- MangoPay/DisputeReasonType.php
+++ MangoPay/DisputeReasonType.php
@@ -12,4 +12,15 @@ class DisputeReasonType
     const Unknown = "UNKNOWN";
     const Other = "OTHER";
     const RefundConversionRate = "REFUND_CONVERSION_RATE";
+    const LateFailureInsufficientFunds = "LATE_FAILURE_INSUFFICIENT_FUNDS";
+    const LateFailureContactUser = "LATE_FAILURE_CONTACT_USER";
+    const LateFailureBankaccountClosed = "LATE_FAILURE_BANKACCOUNT_CLOSED";
+    const LateFailureBankaccountIncompatible = "LATE_FAILURE_BANKACCOUNT_INCOMPATIBLE";
+    const LateFailureBankaccountIncorrect = "LATE_FAILURE_BANKACCOUNT_INCORRECT";
+    const AuthorisationDisputed = "AUTHORISATION_DISPUTED";
+    const TransactionNotRecognized = "TRANSACTION_NOT_RECOGNIZED";
+	const ProductNotProvided = PRODUCT_NOT_PROVIDED";
+	const CanceledReoccuringTransaction = "CANCELED_REOCCURING_TRANSACTION";
+	const RefundNotProcessed = "REFUND_NOT_PROCESSED";
 }
+
