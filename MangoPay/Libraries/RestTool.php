bdd51c662122ec17aa9fd40905adbc8e2ad2601b
Mickaël POIS <mickael@mangopay.com>
2018-11-08 16:37:46 +0100

    Version bump 2.9.2
    Trying to solve authentication issue by removing an extra space into token url.

--- MangoPay/Libraries/RestTool.php
+++ MangoPay/Libraries/RestTool.php
@@ -9,7 +9,7 @@ use Psr\Log\LoggerInterface;
  */
 class RestTool
 {
-    const VERSION = '2.9.1';
+    const VERSION = '2.9.2';
 
     /**
      * Root/parent instance that holds the OAuthToken and Configuration instance
