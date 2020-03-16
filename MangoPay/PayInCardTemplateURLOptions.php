58dd44b110222e37c0b21471fdd484a7edef17bb
Mickaël POIS <mickael@mangopay.com>
2020-03-16 19:34:18 +0100

    Update and rename PayInDirectDebitTemplateURLOptions.php to PayInCardTemplateURLOptions.php

--- MangoPay/PayInCardTemplateURLOptions.php
+++ MangoPay/PayInCardTemplateURLOptions.php
@@ -4,7 +4,7 @@ namespace MangoPay;
 /**
  * Class represents template URL options
  */
-class PayInDirectDebitTemplateURLOptions extends Libraries\Dto
+class PayInCardTemplateURLOptions extends Libraries\Dto
 {
     /**
      * PAYLINE options - Will be deprecated on March 31th (Sandbox) and April 30th (production)
@@ -12,3 +12,10 @@ class PayInDirectDebitTemplateURLOptions extends Libraries\Dto
      * @var string
      */
     public $PAYLINE;
+    
+        /**
+     * PAYLINEV2 options
+     *
+     * @var string
+     */
+    public $PAYLINV2E;
