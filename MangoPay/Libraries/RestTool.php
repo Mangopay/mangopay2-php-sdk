3197f9f73795e1b5ed6113046593a2c8cd6ce7f1
mmuraru-ro <mihai.muraru@softia.ro>
2020-07-17 10:43:53 +0300

    fix

--- MangoPay/Libraries/RestTool.php
+++ MangoPay/Libraries/RestTool.php
@@ -328,7 +328,7 @@ class RestTool
         // Add User-Agent Header
         array_push($this->_requestHttpHeaders, 'User-Agent: MANGOPAY PHP SDK/' . self::VERSION);
         // Add Content-Length
-        array_push($this->_requestHttpHeaders, 'ContentLength: 0');
+        array_push($this->_requestHttpHeaders, 'content-length: ' . ob_get_length());
         // Authentication http header
         if ($this->_authRequired) {
             $authHlp = new AuthenticationHelper($this->_root);
