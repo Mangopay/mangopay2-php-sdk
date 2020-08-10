ae48f80fb78decc18772f384920ac477b9898683
Mihai Muraru <mihai.mtz007@gmail.com>
2020-08-10 10:31:45 +0300

    ...

--- MangoPay/Libraries/RestTool.php
+++ MangoPay/Libraries/RestTool.php
@@ -194,7 +194,7 @@ class RestTool
             }
             // encode to json if needed
             if (in_array(self::$_JSON_HEADER, $httpHeaders)) {
-                if($this->_requestData != null){
+                if(!is_null($this->_requestData)){
                     $this->_requestData = json_encode($this->_requestData);
                     $this->logger->debug('RequestData JSON :' . print_r($this->_requestData, true));
                     if ($this->_root->Config->DebugMode) {
