8dfd734f6550d891805e8e0f73ddb74b2feafdaf
Iulian Masar <iulian.masar@codegile.com>
2023-06-21 09:56:42 +0300

    fixed linter error

--- MangoPay/PayInPaymentDetailsMbway.php
+++ MangoPay/PayInPaymentDetailsMbway.php
@@ -4,10 +4,9 @@ namespace MangoPay;
 
 class PayInPaymentDetailsMbway extends Libraries\Dto implements PayInPaymentDetails
 {
-
     /**
      * Custom description to show on the user's bank statement.
-     * It can be up to 10 char alpha-numeric and space.
+     * It can be up to 10 char alphanumeric and space.
      * @var string
      */
     public $StatementDescriptor;
