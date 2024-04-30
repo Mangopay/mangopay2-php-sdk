b81fb0ba588b0eef814cab1fc1bdbf5de1c35e54
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-04-30 14:22:09 +0600

    chore(composer): Updated to the latest version and config file also

---
+++ .php-cs-fixer.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$finder = (new PhpCsFixer\Finder())
+	->in(__DIR__.'/MangoPay')
+	->in(__DIR__.'/tests')
+;
+
+return (new PhpCsFixer\Config())
+	->setRules([
+		'@PSR12' => true,
+		'fully_qualified_strict_types' => true,
+		'array_syntax' => ['syntax' => 'short'],
+		'visibility_required' => [
+			'elements' => ['property', 'method'], // 'const' is removed for old PHP compat
+		],
+	])
+	->setFinder($finder)
+	;
