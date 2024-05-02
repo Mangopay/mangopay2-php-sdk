96d67ae668e8a0b9cf4f46784852e6c20ec5d994
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-05-02 09:41:38 +0600

    added: label properties for KYC Document

--- MangoPay/KycDocument.php
+++ MangoPay/KycDocument.php
@@ -8,18 +8,33 @@ namespace MangoPay;
 class KycDocument extends Libraries\Document
 {
     /**
+     * The user ID who owns this document
      * @var string
      * @see \MangoPay\KycDocumentType
      */
     public $Type;
 
+	/**
+	 * The label of KYC document type
+	 * @var string
+	 */
+	public $TypeLabel;
+
     /**
+     * The status of the KYC document
      * @var string
      * @see \MangoPay\KycDocumentStatus
      */
     public $Status;
 
+	/**
+	 * The label of KYC document status
+	 * @var string
+	 */
+	public $StatusLabel;
+
     /**
+     * Refused reason type
      * @var string
      */
     public $UserId;
