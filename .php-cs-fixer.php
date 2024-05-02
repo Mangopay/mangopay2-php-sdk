87bfb35c918c5c93367b9b8ba8f83927eb9c6cd7
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-05-02 10:55:26 +0600

    update: php cs fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,18 +1,18 @@
 <?php
 
 $finder = (new PhpCsFixer\Finder())
-	->in(__DIR__.'/MangoPay')
-	->in(__DIR__.'/tests')
+    ->in(__DIR__.'/MangoPay')
+    ->in(__DIR__.'/tests')
 ;
 
 return (new PhpCsFixer\Config())
-	->setRules([
-		'@PSR12' => true,
-		'fully_qualified_strict_types' => true,
-		'array_syntax' => ['syntax' => 'short'],
-		'visibility_required' => [
-			'elements' => ['property', 'method'], // 'const' is removed for old PHP compat
-		],
-	])
-	->setFinder($finder)
-	;
+    ->setRules([
+        '@PSR12' => true,
+        'fully_qualified_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'visibility_required' => [
+            'elements' => ['property', 'method'], // 'const' is removed for old PHP compat
+        ],
+    ])
+    ->setFinder($finder)
+;
