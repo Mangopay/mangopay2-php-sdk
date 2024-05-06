d77e72bab3881906dfd44957f5efd920c6a47653
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2024-05-06 14:11:28 +0600

    remove: shell path from docker composer

--- php_env/PHP_8.2/composer.sh
+++ php_env/PHP_8.2/composer.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
