21b90eefe2bfe99871836484f13a0c2d411a781b
Iulian Masar <iulian.masar@codegile.com>
2024-07-30 16:36:18 +0300

    version bump 3.31.0

--- MangoPay/Libraries/RestTool.php
+++ MangoPay/Libraries/RestTool.php
@@ -11,7 +11,7 @@ use Psr\Log\LoggerInterface;
  */
 class RestTool
 {
-    const VERSION = '3.30.0';
+    const VERSION = '3.31.0';
 
     /**
      * Root/parent instance that holds the OAuthToken and Configuration instance
