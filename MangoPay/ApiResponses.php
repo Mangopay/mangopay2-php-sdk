39e09c6c7c9f97242e80629327524b5e9a98db64
William Desportes <williamdes@wdes.fr>
2024-08-27 14:49:53 +0200

    Fix idempotency response should return an Error object

--- MangoPay/ApiResponses.php
+++ MangoPay/ApiResponses.php
@@ -16,12 +16,20 @@ class ApiResponses extends Libraries\ApiBase
     public function Get($idempotencyKey)
     {
         $response = $this->GetObject('responses_get', 'MangoPay\Response', $idempotencyKey);
-        $className = $this->GetObjectForIdempotencyUrl($response->RequestURL);
-        if (is_null($className) || empty($className) || is_null($response->Resource) || empty($response->Resource)) {
+
+        // Same logic as RestTool::CheckResponseCode
+        if ($response->StatusCode >= 200 && $response->StatusCode <= 299) {
+            // Target the class to use from the SDK
+            $className = $this->GetObjectForIdempotencyUrl($response->RequestURL);
+            if (is_null($className) || empty($className) || is_null($response->Resource) || empty($response->Resource)) {
+                return $response;
+            }
+
+            $response->Resource = $this->CastResponseToEntity($response->Resource, $className);
             return $response;
         }
 
-        $response->Resource = $this->CastResponseToEntity($response->Resource, $className);
+        $response->Resource = $this->CastResponseToError($response->Resource);
         return $response;
     }
 }
