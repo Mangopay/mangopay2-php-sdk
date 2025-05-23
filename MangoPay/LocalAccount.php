1bb5bb552588d362dc5021d2216c857f2fc1e687
Iulian Masar <iulian.masar@codegile.com>
2025-05-23 14:57:11 +0300

    handle virtual account changes

--- MangoPay/LocalAccount.php
+++ MangoPay/LocalAccount.php
@@ -15,4 +15,39 @@ class LocalAccount extends Libraries\Dto
      * @var string
      */
     public $SortCode;
+
+    /**
+     * @var string
+     */
+    public $Iban;
+
+    /**
+     * @var string
+     */
+    public $Bic;
+
+    /**
+     * @var string
+     */
+    public $AchNumber;
+
+    /**
+     * @var string
+     */
+    public $FedWireNumber;
+
+    /**
+     * @var string
+     */
+    public $AccountType;
+
+    /**
+     * @var string
+     */
+    public $BranchCode;
+
+    /**
+     * @var string
+     */
+    public $InstitutionNumber;
 }
