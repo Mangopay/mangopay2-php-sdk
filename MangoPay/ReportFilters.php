ad6debcbf48e5969446e804aebb462c7fd9ffbea
Iulian Masar <iulian.masar@codegile.com>
2025-05-29 11:05:43 +0300

    updated docs

--- MangoPay/ReportFilters.php
+++ MangoPay/ReportFilters.php
@@ -8,19 +8,19 @@ namespace MangoPay;
 class ReportFilters extends Libraries\Dto
 {
     /**
-     * The currency of the wallet(s) being credited or debited by the report’s transactions.
+     * The currency of the DebitedFunds, CreditedFunds, or Fees of the transactions (and therefore the wallets).
      * @var string
      */
     public $Currency;
 
     /**
-     * The unique identifier of the user owning the wallet(s).
+     * The unique identifier of the user referenced as the AuthorId or CreditedUserId of the transaction.
      * @var string
      */
     public $UserId;
 
     /**
-     * The unique identifier of the wallet.
+     * The unique identifier of the wallet referenced as the DebitedWalletId or CreditedWalletId of the transaction.
      * @var string
      */
     public $WalletId;
