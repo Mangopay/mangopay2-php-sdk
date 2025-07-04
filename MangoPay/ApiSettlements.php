b3ac0d4259a5e309bdc173a86abe848135a71cbc
Iulian Masar <iulian.masar@codegile.com>
2025-07-04 16:13:25 +0300

    handle get and update of settlement

--- MangoPay/ApiSettlements.php
+++ MangoPay/ApiSettlements.php
@@ -8,6 +8,7 @@ namespace MangoPay;
 class ApiSettlements extends Libraries\ApiBase
 {
     /**
+     * Upload a settlement file
      * @param string $file The file to be uploaded (binary string)
      * @param string $idempotencyKey Idempotency key
      * @return Settlement Object returned by the API
@@ -17,5 +18,30 @@ class ApiSettlements extends Libraries\ApiBase
     {
         return $this->CreateOrUpdateMultipartObject('settlement_create', $file, "settlement_file.csv",
             '\MangoPay\Settlement', null, $idempotencyKey);
+    }
+
+    /**
+     * Get a settlement
+     * @param string $settlementId
+     * @return Settlement Recipient object returned from API
+     * @throws Libraries\Exception
+     */
+    public function Get($settlementId)
+    {
+        return $this->GetObject('settlement_get', '\MangoPay\Settlement', $settlementId);
+    }
+
+    /**
+     * Update a settlement file
+     * @param string $settlementId
+     * @param string $file The file to be uploaded (binary string)
+     * @param string $idempotencyKey Idempotency key
+     * @return Settlement Object returned by the API
+     * @throws Libraries\Exception
+     */
+    public function Update($settlementId, $file, $idempotencyKey = null)
+    {
+        return $this->CreateOrUpdateMultipartObject('settlement_update', $file, "settlement_file.csv",
+            '\MangoPay\Settlement', $settlementId, $idempotencyKey);
     }
 }
--- tests/Cases/SettlementsTest.php
+++ tests/Cases/SettlementsTest.php
@@ -18,6 +18,27 @@ class SettlementsTest extends Base
         self::assertEquals("UPLOADED", $created->Status);
     }
 
+    public function test_Settlements_Get()
+    {
+        $created = $this->createNewSettlement();
+        sleep(10);
+        $fetched = $this->_api->Settlements->Get($created->SettlementId);
+        self::assertNotNull($fetched);
+        self::assertEquals("PARTIALLY_SETTLED", $fetched->Status);
+    }
+
+    public function test_Settlements_Update()
+    {
+        $created = $this->createNewSettlement();
+        self::assertEquals("UPLOADED", $created->Status);
+        $file = file_get_contents(__DIR__ . '/../settlement_sample.csv');
+        $updated = $this->_api->Settlements->Update($created->SettlementId, $file);
+        self::assertEquals("UPLOADED", $updated->Status);
+        sleep(10);
+        $fetched = $this->_api->Settlements->Get($updated->SettlementId);
+        self::assertEquals("PARTIALLY_SETTLED", $fetched->Status);
+    }
+
     private function createNewSettlement()
     {
         if (self::$Settlement != null) {
